b34d7413d3aed0a6fc4d0dbeb8373006fd82d5e9
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-07-07 22:56:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,9 @@ def my_hash
 end
 
 def pioneer
-  strong = {}
+  pioneer = {
+    
+  }
   strong[:name] = "Grace Hopper"
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
